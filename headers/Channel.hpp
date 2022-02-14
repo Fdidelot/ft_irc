93b35d98008c96672f253363af120f122f75c74b
Pierre SEMSARI NEJAD <psemsari@e2r9p15.clusters.42paris.fr>
2022-02-14 17:39:44 +0100

    channel modif

--- headers/Channel.hpp
+++ headers/Channel.hpp
@@ -6,7 +6,7 @@
 /*   By: psemsari <psemsari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 15:05:02 by psemsari          #+#    #+#             */
-/*   Updated: 2022/02/11 17:21:51 by psemsari         ###   ########.fr       */
+/*   Updated: 2022/02/14 17:36:45 by psemsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,21 @@ class User;
 class Channel
 {
 	public:
+		typedef std::list<User *> users_list;
+
+	public:
 		Channel(std::string name);
 		~Channel();
+		std::string getName();
+		void addToChannel(User *user);
+		void addOpToChannel(User *user);
+		void removeFromChannel(User *user);
+		void sendToChannel(std::string str);
+
 	private:
 		std::string _name;
-		std::list<User *> _users;
-		std::list<User *> _opers;
+		users_list	_users;
+		users_list	_opers;
 };
 
 #endif
