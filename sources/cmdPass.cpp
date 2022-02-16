3654674c01c9c9fb00e68baf93a1f88e739b30a9
benj0t <bmoreau89000@gmail.com>
2022-02-16 17:40:31 +0100

    update

--- sources/cmdPass.cpp
+++ sources/cmdPass.cpp
@@ -21,7 +21,6 @@ void	Command::_pass(std::stringstream& completeCommand, User& user) {
 		completeCommand >> pass;
 		if (!pass.empty())
 			sendCommand(user, 462, ERR_NEEDMOREPARAMS(cmd));
-		std::cout << "Pass: " << pass << std::endl;
 		if (pass != user.getServer().getPassword())
 			user.getServer().endConnection(user.getFd());
 	}
