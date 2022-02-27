0833c14fec2dcbb6c10c6ae68e39680074090ee2
Felicien DIDELOT <fdidelot@e2r10p10.clusters.42paris.fr>
2022-02-27 19:53:13 +0100

    minor changes, cmdKill codes added and SEND_OPT move from Command.hpp to Server.hpp

--- headers/Command.hpp
+++ headers/Command.hpp
@@ -16,7 +16,6 @@
 # include "User.hpp"
 # define NB_COMMAND 16 //add if command
 # define EOT_CODE 4
-# define SEND_OPT 0
 
 // Error codes
 # define ERRCODE_NOSUCHNICK			401
--- headers/Server.hpp
+++ headers/Server.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Server.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: psemsari <psemsari@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fdidelot <fdidelot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/27 15:58:08 by fdidelot          #+#    #+#             */
-/*   Updated: 2022/02/22 13:48:10 by psemsari         ###   ########.fr       */
+/*   Updated: 2022/02/27 18:18:58 by fdidelot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,6 +57,7 @@
 # define FAILURE_SELECT 4
 # define ERROR -1
 # define BACKLOG 10   // how many pending connections queue will hold
+# define SEND_OPT 0
 
 # include "Command.hpp"
 # include "User.hpp"
